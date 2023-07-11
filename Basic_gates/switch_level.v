7d5bf3d68ec8fcfb01821f12c527f53de56e4338
Venkata Sai Hari Krishna Maddi <67937695+mvsharikrishna@users.noreply.github.com>
2023-07-11 12:08:34 +0530

    Create switch_level.v

---
+++ Basic_gates/switch_level.v
@@ -0,0 +1,11 @@
+module basic_gates_switch(input a_in,
+                         input b_in,
+                         output not_out,
+                         output buf_out,
+                         output and_out,
+                         output or_out,
+                         output nand_out,
+                         output nor_out,
+                         output xor_out,
+                         output xnor_out);
+  reg 
